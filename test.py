3605026fc0248846e31abbd252d8e62e41e075b5
gilleslenaerts007 <gilleslenaerts007@gmail.com>
2019-11-08 15:51:17 +0100

    aanpassing

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import gym
+#import pybulletgym  # register PyBullet enviroments with open ai gym
+
 env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(200):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+observation = env.reset()
+for _ in range(100):
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
+
+  #if done:
+    #observation = env.reset()
 env.close()
